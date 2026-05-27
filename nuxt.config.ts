a8be33dbf8f4cb86490128e6632eda139c325f56
xalidevxx <xalidevxx@gmail.com>
2026-05-27 21:56:54 +0500

    Remove unsupported i18n bundle option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -100,9 +99,6 @@ export default defineNuxtConfig({
         dir: 'ltr',
       },
     ],
-    bundle: {
-      optimizeTranslationDirective: false,
-    },
   },
   routeRules: {
     '/': { prerender: true },
