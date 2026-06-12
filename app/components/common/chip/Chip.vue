29af626e000a041df909ced9d898f27eec3b22ac
Alikhan <xalidevxx@gmail.com>
2026-06-13 03:06:55 +0500

    Refactor code structure for improved readability and maintainability

--- app/components/common/chip/Chip.vue
+++ app/components/common/chip/Chip.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="h-9 px-3.5 flex items-center justify-center bg-input/30 text-card-foreground border dark:border-none rounded-xl text-md font-normal tracking-wide"
+		class="h-9 px-3.5 flex items-center justify-center bg-secondary/30 text-card-foreground border dark:border-none rounded-xl text-md font-normal tracking-wide"
 	>
 		<slot />
 	</div>
--- app/components/layout/cv/CVButton.vue
+++ app/components/layout/cv/CVButton.vue
@@ -3,7 +3,14 @@ import { ExternalLink } from '@lucide/vue';
 </script>
 
 <template>
-	<Button size="lg" color="secondary" variant="outlined" class="mx-auto">
+	<Button
+		size="lg"
+		color="secondary"
+		variant="outlined"
+		class="mx-auto w-fit"
+		href="/CV-Alikhan.pdf"
+		target="_blank"
+	>
 		{{ $t('about.cta') }}
 		<template #trailing>
 			<ExternalLink />
