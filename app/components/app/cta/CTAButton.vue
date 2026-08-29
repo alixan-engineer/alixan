5a03384ac20b1bcd8de5d7f1cddb19e0f0128dc9
Alikhan <xalidevxx@gmail.com>
2026-08-29 18:07:13 +0500

    feat: add CTADialog component with contact links and icons
    - Created a new CTADialog.vue component for displaying contact options.
    - Added links for WhatsApp, Telegram, and Gmail with corresponding SVG icons.
    - Implemented a close button to dismiss the dialog.
    - Included translations for dialog title and close button text.
    
    chore: add SVG icons for contact methods
    
    - Added Gmail, Telegram, and WhatsApp SVG icons to the project.
    - Ensured icons are properly formatted and optimized for use in the CTADialog component.

--- app/components/app/cta/CTAButton.vue
+++ app/components/app/cta/CTAButton.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
 import { Zap } from '@lucide/vue';
+import CTADialog from './CTADialog.vue';
+
+const dialog = useDialog();
+
+const openDialog = () => {
+	dialog.open(CTADialog, {
+		width: '520px',
+		height: '520px',
+	});
+};
 </script>
 
 <template>
-	<Button size="responsive" variant="filled" color="primary">
+	<Button
+		size="responsive"
+		variant="filled"
+		color="primary"
+		@click="openDialog"
+	>
 		<template #leading>
 			<Zap />
 		</template>
---
+++ app/components/app/cta/CTADialog.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+const props = defineProps<{
+	close: () => void;
+}>();
+
+const list = [
+	{
+		icon: '/img/contacts/wa.svg',
+		link: 'https://wa.me/alixan.engineer',
+		label: '@alixan.engineer',
+	},
+	{
+		link: 'https://t.me/alixan_engineer',
+		icon: '/img/contacts/tg.svg',
+		label: '@alixan_engineer',
+	},
+	{
+		link: 'mailto:alixan.engineer@gmail.com',
+		icon: '/img/contacts/gmail.svg',
+		label: 'alixan.engineer@gmail.com',
+	},
+] as const;
+</script>
+
+<template>
+	<div class="size-full flex flex-col gap-6 px-6 py-8">
+		<p class="text-2xl font-semibold text-center">
+			{{ $t('home.cta.dialog.title') }}
+		</p>
+
+		<div class="flex-1" />
+
+		<div class="space-y-5">
+			<a
+				v-for="(item, i) in list"
+				:key="i"
+				:href="item.link"
+				target="_blank"
+				class="w-full p-4 flex items-center gap-5 cursor-pointer rounded-2xl border bg-card"
+			>
+				<img
+					:src="item.icon"
+					:alt="$t('home.cta.dialog.contactIconAlt')"
+					loading="lazy"
+					class="size-11 object-contain"
+				/>
+				<span class="text-xm font-medium">{{ item.label }}</span>
+			</a>
+		</div>
+
+		<div class="flex-1" />
+
+		<Button variant="filled" color="primary" @click="props.close()">
+			{{ $t('home.cta.dialog.close') }}
+		</Button>
+	</div>
+</template>
